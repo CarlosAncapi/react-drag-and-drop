d5c1122dceec2495fb4a484de3e43e0a23b379b3
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-02-07 18:48:56 -0300

    ordenando el array con OnDragEnd y HandleDragEnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,58 @@
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import { useState } from "react";
 
-const initialTodos= [
-  {id:1, text:'Aprender React'},
-  {id:2, text:'Aprender JavaScript'},
-  {id:3, text:'Aprender Vue.js'},
-  {id:4, text:'Aprender Angular'},
-]
+const initialTodos = [
+  { id: 1, text: "Aprender React" },
+  { id: 2, text: "Aprender JavaScript" },
+  { id: 3, text: "Aprender Vue.js" },
+  { id: 4, text: "Aprender Angular" },
+];
 
 const App = () => {
-  const [todos,setTodos] = useState(initialTodos)
+  const [todos, setTodos] = useState(initialTodos);
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const startIndex = result.source.index;
+    const endIndex = result.destination.index;
+
+    const copyArray = [...todos];
+    const [reorderItem] = copyArray.splice(startIndex, 1);
+
+    copyArray.splice(endIndex, 0, reorderItem);
+
+    setTodos(copyArray);
+  };
 
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <h1>Todo app</h1>
       <Droppable droppableId="todos">
-        {
-          (DroppableProvider) => (
-            <ul 
-              ref={DroppableProvider.innerRef} 
-              {...DroppableProvider.droppableProps}
-            >
-              {
-                todos.map( ( todo, index )=> (
-                  <Draggable 
-                    index={index}
-                    key={todo.id} 
-                    draggableId={`${todo.id}`}
+        {(DroppableProvider) => (
+          <ul
+            ref={DroppableProvider.innerRef}
+            {...DroppableProvider.droppableProps}
+          >
+            {todos.map((todo, index) => (
+              <Draggable index={index} key={todo.id} draggableId={`${todo.id}`}>
+                {(draggableProvider) => (
+                  <li
+                    ref={draggableProvider.innerRef}
+                    {...draggableProvider.dragHandleProps}
+                    {...draggableProvider.draggableProps}
                   >
-                    {
-                      (draggableProvider) => (
-                        <li 
-                          ref={draggableProvider.innerRef}
-                          {...draggableProvider.dragHandleProps}
-                          {...draggableProvider.draggableProps}
-                        >
-                          {todo.text}
-                        </li>
-                      )
-                    }
-                  </Draggable>
-                ))
-              }
-              
-              {DroppableProvider.placeholder}
-              
-            </ul>
-          )
-        }
+                    {todo.text}
+                  </li>
+                )}
+              </Draggable>
+            ))}
+
+            {DroppableProvider.placeholder}
+          </ul>
+        )}
       </Droppable>
     </DragDropContext>
-  )
-
+  );
 };
 export default App;
